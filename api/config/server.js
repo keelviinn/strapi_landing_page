f9f4c3f3d71bc50ef9e8462b3f1b9e489064e8fb
Kelvin Oliveira <keelviinn@gmail.com>
2020-11-27 21:49:02 -0300

    fisrt commit

---
+++ api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e7c1661f426df9bd9676deddb1d219a1'),
+    },
+  },
+});
---
+++ api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '23dd0c65-797e-4396-b926-49ec68e55e12'
+};
